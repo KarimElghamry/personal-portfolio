89e766d4d1d605c177b2ad8b698870404e414ee0
KarimElghamry <Karimelghamry@gmail.com>
2022-03-25 19:08:50 +0200

    add: chillify image

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,7 +1,9 @@
 import logo from "./images/SlimXV-black.png";
 import personalImage from "./images/personal-bw.png";
+import chillify from "./images/chillify.png";
 
 export const images = {
   logo: logo,
   personalImage: personalImage,
+  chillify: chillify,
 };
