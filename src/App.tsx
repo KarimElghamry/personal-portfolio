660a20eb2f13915cb40720c5d813e150e5d6ecee
KarimElghamry <Karimelghamry@gmail.com>
2022-03-25 12:00:27 +0200

    fix: global styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+import Header from "./components/Header";
+import { routes } from "./data/routes";
 
 const App: React.FC = () => {
-  return <div></div>;
+  return (
+    <>
+      <Header routes={routes}></Header>
+    </>
+  );
 };
 
 export default App;
---
+++ src/assets/index.ts
@@ -0,0 +1,5 @@
+import logo from "./images/SlimXV-black.png";
+
+export const images = {
+  logo: logo,
+};
