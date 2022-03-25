35605110f263f9b6aa74b0d5ecb001c600fd18bd
KarimElghamry <Karimelghamry@gmail.com>
2022-03-25 19:09:24 +0200

    add: global constants

---
+++ src/data/constants.ts
@@ -0,0 +1,42 @@
+import { images } from "../assets";
+import Project from "../models/Project";
+
+export const routes = ["Home", "Personal Projects", "About", "Contact"];
+
+export const personalProjects: Project[] = [
+  {
+    title: "Chillify - Music App",
+    description: `
+        An offline music player mobile application established using
+        Flutter and Dart. Currently, Chillify has 550+ stars on Github and is 
+        listed as one of the top open source Flutter apps made
+        by the community.
+    `,
+    image: images.chillify,
+    url: "https://github.com/KarimElghamry/chillify",
+  },
+
+  {
+    title: "Chillify - Music App",
+    description: `
+        An offline music player mobile application established using
+        Flutter and Dart. Currently, Chillify has 550+ stars on Github and is 
+        listed as one of the top open source Flutter apps made
+        by the community.
+    `,
+    image: images.chillify,
+    url: "https://github.com/KarimElghamry/chillify",
+  },
+
+  {
+    title: "Chillify - Music App",
+    description: `
+        An offline music player mobile application established using
+        Flutter and Dart. Currently, Chillify has 550+ stars on Github and is 
+        listed as one of the top open source Flutter apps made
+        by the community.
+    `,
+    image: images.chillify,
+    url: "https://github.com/KarimElghamry/chillify",
+  },
+];
